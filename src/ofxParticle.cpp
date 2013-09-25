b56c05bc6c4ae0f1b4d0de0cc6d24dd2f6267359
smallfly <hbruyere@gmail.com>
2013-09-25 13:21:26 -0400

    updating the particle's colour alpha in the update

--- src/ofxParticle.cpp
+++ src/ofxParticle.cpp
@@ -171,6 +171,8 @@ void ofxParticle::update(const float timeStep, const float drag)
     acceleration -= acceleration * dt;
     rotation += rotationalVelocity * dt;
     
+    color.a = life/lifeStart*color.a;
+    
     life -= dt;
 }
 
