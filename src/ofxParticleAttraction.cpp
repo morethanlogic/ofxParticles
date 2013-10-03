ff1afd0ee58cf052d7d53523874142c4ff06dabf
smallfly <hbruyere@gmail.com>
2013-10-03 17:32:28 -0400

    Drawing nice circles to represent the force in 3D.

--- src/ofxParticleAttraction.cpp
+++ src/ofxParticleAttraction.cpp
@@ -56,7 +56,24 @@ void ofxParticleAttraction::debugDraw(int r, int g, int b)
 {
     ofxParticleForce::debugDraw(r, g, b, 128);
     ofSetColor(r, g, b);
+    
+    ofSetCircleResolution(50);
     ofNoFill();
-    ofCircle(position, minDist);
-    ofCircle(position, maxDist);
+    ofPushMatrix();
+    ofTranslate(position);
+    
+    ofCircle(.0f, .0f, .0f, minDist);
+    ofCircle(.0f, .0f, .0f, maxDist);
+    
+    ofRotate(90.0f, .0f, 1.0f, 0.0f);
+    ofCircle(.0f, .0f, .0f, minDist);
+    ofCircle(.0f, .0f, .0f, maxDist);
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    ofTranslate(position);
+    ofRotate(90.0f, 1.0f, .0f, 0.0f);
+    ofCircle(.0f, .0f, .0f, minDist);
+    ofCircle(.0f, .0f, .0f, maxDist);
+    ofPopMatrix();
 }
